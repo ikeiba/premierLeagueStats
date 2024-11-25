a86ac5c80146da899ba1832123fa070ff2af2bae
ikeiba <ibarrola.iker@opendeusto.es>
2024-11-25 09:51:00 +0100

    Update Premier_League_Statistics.R

--- Premier_League_Statistics.R
+++ Premier_League_Statistics.R
@@ -1,6 +1,4 @@
 #DELIVERABLE 2: Asier Gomez, Enetz Quindimil, Iker Ibarrola
 #In this deliverable we will be analysing different Premier League Statistics from the 2023/2024 season
 
-getwd()
-
 print(read.csv("data/accurate_cross_team.csv"))
